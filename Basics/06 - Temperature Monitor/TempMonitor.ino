6810d0f78dd1c3c14dc8c7c2f33a552d633b24c4
MarquezLuis96 <englamc@gmail.com>
2021-04-08 13:35:55 -0400

    V.0.0.1.8 - Printing in a LCD Screen

--- Basics/06 - Temperature Monitor/TempMonitor.ino
+++ Basics/06 - Temperature Monitor/TempMonitor.ino
@@ -7,13 +7,25 @@
 
 int tempC;
 const int sensorTemp = A0;
+liquidCrystal lcd (12, 11, 5, 4, 3, 2);
 
 void setup() {
     Serial.begin(9600);
+    lcd.begin(16, 2);
 }
 
 void loop() {
     tempC = analogRead(sensorTemp);
     tempc = tempC * 0.48828125;
     Serial.println(tempC);
+    lcd.setCursor(0,0);
+    lcd.print("Temperatura:");
+    lcd.setCursor(1,0);
+    lcd.print(tempC);
+    delay(1000);
+    lecd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Temperature:");
+    lcd.setCursor(1,0);
+    lcd.print(tempC);
 }
